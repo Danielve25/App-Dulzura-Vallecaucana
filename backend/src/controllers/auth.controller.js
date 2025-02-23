7aee84d62d770a730779678236e6c231aab329a7
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-02-23 02:11:23 -0500

    Añadir registro de usuarios

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,20 @@
-export const register = (req, res) => {
-  res.send("register");
+import User from "../models/user.model.js";
+
+export const register = async (req, res) => {
+  const { NameStudent, PhoneNumber } = req.body;
+
+  try {
+    const NewUser = new User({
+      NameStudent,
+      PhoneNumber,
+    });
+
+    const userSaved = await NewUser.save();
+    res.json(userSaved);
+  } catch (error) {
+    console.log("ya existe este usuario");
+    res.send("ya existe este usuario");
+  }
 };
 export const login = (req, res) => {
   res.send("login");
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 import { Temporal } from "temporal-polyfill";
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     NameStudent: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     PhoneNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    DateCreated: {
-        type: String,
-        default: () => Temporal.Now.instant().toString()
-    }
-})
-export default mongoose.model('User', userSchema);
+  },
+  {
+    timestamps: true,
+  }
+);
+export default mongoose.model("User", userSchema);
