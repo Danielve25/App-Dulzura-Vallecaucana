af5b982365e64d1deacc88d2de24eacceab692af
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-03-02 18:20:09 -0500

    añadir validacion del token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,3 +65,16 @@ export const logout = (req, res) => {
   });
   return res.sendStatus(200);
 };
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+  if (!userFound)
+    return res.status(400).json({ message: "Usuario no encontrado" });
+
+  return res.json({
+    id: userFound._id,
+    NameStudent: userFound.NameStudent,
+    creastedAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
+};
