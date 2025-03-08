3c875cb043f6f057e3a25cd98e76bcf7e94a9e77
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-03-08 13:51:46 -0500

    Añadir contexto de usuarios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { AuthProvider } from "./context/AuthContext";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/tasks" element={<h1>tasks</h1>} />
-        <Route path="/add-task" element={<h1>add Task</h1>} />
-        <Route path="/task/:id" element={<h1>update task</h1>} />
-        <Route path="/profile" element={<h1>profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>tasks</h1>} />
+          <Route path="/add-task" element={<h1>add Task</h1>} />
+          <Route path="/task/:id" element={<h1>update task</h1>} />
+          <Route path="/profile" element={<h1>profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,34 +1,69 @@
 import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
 function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signup, isAuthenticated, errors: RegisterErrors } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/tasks");
+  }, [isAuthenticated]);
 
   const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res);
+    signup(values);
   });
 
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      {RegisterErrors.map((error, i) => (
+        <div key={i} className="bg-red-500 p-2 text-white">
+          {error}
+        </div>
+      ))}
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          {...register("NameStudent", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
-        />
-        <input
-          type="text"
-          placeholder="Phone Number"
-          {...register("PhoneNumber", {
-            required: true,
-            pattern: /^[0-9]*$/, // Solo permite números
-          })}
-          onInput={(e) => {
-            e.target.value = e.target.value.replace(/\D/g, ""); // Elimina caracteres no numéricos
-          }}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-3"
-        />
+        <div className="mb-[3px]">
+          <label htmlFor="NameStudent" className="label">
+            Nombre
+          </label>
+          <input
+            name="NameStudent"
+            type="text"
+            placeholder="Nombre"
+            {...register("NameStudent", { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md "
+          />
+          {errors.NameStudent && (
+            <p className="text-red-500">Nombre es requerido</p>
+          )}
+        </div>
+        <div className="mb-[3px]">
+          <label htmlFor="PhoneNumber" className="label">
+            Numero de telefono
+          </label>
+          <input
+            type="text"
+            name="PhoneNumber"
+            placeholder="Phone Number"
+            {...register("PhoneNumber", {
+              required: true,
+              pattern: /^[0-9]*$/, // Solo permite números
+            })}
+            onInput={(e) => {
+              e.target.value = e.target.value.replace(/\D/g, ""); // Elimina caracteres no numéricos
+            }}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md "
+          />
+          {errors.PhoneNumber && (
+            <p className="text-red-500">numero de telefono es requerido</p>
+          )}
+        </div>
 
         <button className="cursor-pointer" type="submit">
           Register
