ebcac6d584b2c044cb797dbb4f5f6b6677fde87c
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-03-24 18:09:28 -0500

    empezar desarrolo en android del proyecto (ya se añadio estaticamente el login page)

---
+++ Android-App/screens/Register.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+export default function Register() {
+  return (
+    <View>
+      <Text>Register</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
