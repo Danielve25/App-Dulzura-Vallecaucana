ffc5fe95faf4d9d8be436c2af0a33da1a4029450
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-04-20 12:04:25 -0500

    feat: añadir soporte para fuentes personalizadas con unplugin-fonts en la configuración de Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,25 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Fonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Fonts({
+      google: {
+        families: [
+          {
+            name: "Roboto",
+            styles: "wght@400;700",
+          },
+          "Open Sans",
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
