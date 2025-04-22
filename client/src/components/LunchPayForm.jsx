00addbbd3041b99068338b92ea6a2dcb9fd6ab88
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-04-21 20:36:34 -0500

    cambio menor al css

--- client/src/components/LunchPayForm.jsx
+++ client/src/components/LunchPayForm.jsx
@@ -61,7 +61,7 @@ const LunchPayForm = ({ setIsOpen, id_task, payAmount }) => {
             onClick={() => setIsOpen(false)}
             className="absolute top-0 right-0 mt-2 mr-2 cursor-pointer"
           >
-            <X className="mb-2" />
+            <X className="m-2" />
           </button>
           <h1 className="text-2xl font-bold">Pagar Almuerzo</h1>
         </header>
